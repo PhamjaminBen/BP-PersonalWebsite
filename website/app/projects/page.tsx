897780842452fa308e5b146c9218947855272f35
Ben <ben58630@gmail.com>
2023-07-15 23:37:32 -0700

    added grid

--- website/app/projects/page.tsx
+++ website/app/projects/page.tsx
@@ -6,21 +6,22 @@ export default async function ProjectsPage(){
   console.log(projects)
   return(
     <>
-      <h1 className="text-5xl text-center font-inter pt-5 space-y-5">Projects</h1>
-        <ul>
+      <h1 className="text-5xl text-center font-inter pt-5 space-y-5 mb-16">Projects</h1>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-8 lg:gap-y-16 gap-x-0 mb-10">
           {projects.map((project,index) => (
-            <li key = {`${index}`} className="flex flex-col space-y-10 lg:space-y-0 lg:space-x-4 lg:flex-row mx-10 sm:mx-16 md:mx-24 my-16">
-              <div className="w-full lg:w-1/2">
+            <>
+              <div className=" mx-8 lg:ml-16">
                 <h2 className="text-xl mr-10">{project.title}</h2>
                 <br/>
                 <article dangerouslySetInnerHTML={{__html: project.body}} className="mb-5 "/>
                 <a href= {project.github} className='px-3 py-1 object-center bg-gray-500 border-gray-500 rounded-md border-4 text-black font-bold hover:h-10 hover:w-36'>Github Repo</a>
               </div>
-              <Image className="w-full lg:w-1/2 lg:h-1/2" src = {project.image} alt = "face detect"></Image>
-
-            </li>
+              <div className="mx-8 mb-12 lg:mr-16 ">
+                <Image className="w-full" src = {project.image} alt = "face detect" width={1000} height={1000}></Image>
+              </div>
+            </>
           ))}
-        </ul>
+        </div>
     </>
   )
 }
