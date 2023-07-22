f962f396f1c3cfbf6af07a5429437c61ae12883a
Ben <ben58630@gmail.com>
2023-07-21 18:26:35 -0700

    fixed resume page

---
+++ website/app/resume/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+// import resume from '@/public/benphamresume.pdf'
+
+export default function Resume(){
+  return (
+    <>
+      <iframe src="/benphamresume.pdf" className="w-full h-screen"></iframe>
+    </>
+  )
+}
