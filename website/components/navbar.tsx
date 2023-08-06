ec9ba253ffe3924b1567918ee61c2725476331c0
Ben Pham <ben58630@gmail.com>
2023-08-06 14:12:45 -0700

    changed navbar width

--- website/components/navbar.tsx
+++ website/components/navbar.tsx
@@ -1,18 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 export default function NavBar() {
-  return (
-    <nav className="bg-white text-black font-semibold px-4 py-5 text-lg lg:px-12 w-full shadow-md">
-      <ul className="flex gap-8">
-        <li>
-          <Link href="/" className="font-extrabold hover:underline text-xl">Ben.dev</Link>
-        </li>
-        <li className="ml-auto">
-          <Link href="/" className="hover:underline">Home</Link>
-        </li>
-        <li>
-          <Link href="/projects" className="hover:underline">Projects</Link>
-        </li>
-      </ul>
-  </nav>
-  )
+	return (
+		<nav className='bg-white text-black font-semibold text-lg w-full shadow-md'>
+			<div id='content' className='max-w-6xl py-5 px-5 m-auto text-lg'>
+				<ul className='flex gap-8'>
+					<li>
+						<Link href='/' className='font-extrabold hover:underline text-xl'>
+							Ben.dev
+						</Link>
+					</li>
+					<li className='ml-auto'>
+						<Link href='/' className='hover:underline'>
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link href='/projects' className='hover:underline'>
+							Projects
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 }
